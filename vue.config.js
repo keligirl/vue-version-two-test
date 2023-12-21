fcba3004ad337bb9a9950f5fbfb3594ec63f698e
yinyue <2104462898@qq.com>
2023-12-21 10:55:12 +0800

    vue2项目本地调试开启https，加载本地证书

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+const path = require('path')
+const fs = require('fs')
+module.exports = {
+  devServer: {
+    open: true,
+    https: {
+      cert: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.crt')),
+      key: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.key'))
+    }
+  }
+}
