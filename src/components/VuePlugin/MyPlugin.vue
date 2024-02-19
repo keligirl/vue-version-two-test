fd4a591fd007926376be56f4b33cbbc76990477c
ocean <2104462898@qq.com>
2024-02-19 22:58:22 +0800

    自定义插件

--- src/components/VuePlugin/MyPlugin.vue
+++ src/components/VuePlugin/MyPlugin.vue
@@ -27,12 +27,19 @@ export default {
 <style>
 .button {
   /* 样式 */
+  border-radius: 5px;
+  width: 90px;
+  height: 30px;
+  font-size: 32px;
+  color: #fff;
 }
 .default {
   /* 默认样式 */
+  background-color: aqua;
 }
 .primary {
   /* 主要样式 */
+  background-color: antiquewhite;
 }
 /* 其他样式 */
 </style>
