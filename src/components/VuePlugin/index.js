bbe1d0366023743f401deac1e21ed3f018e33fa7
ocean <2104462898@qq.com>
2024-02-19 23:19:48 +0800

    custom-plugin

--- src/components/VuePlugin/index.js
+++ src/components/VuePlugin/index.js
@@ -1,10 +1,15 @@
+import MyPluginButton from "./MyPlugin.vue";
 
-import MyPluginButton from "./MyPlugin.vue"
-
-const MyPlugin= {
+const MyPlugin = {
   install(Vue) {
-    Vue.component("vue-plugin",MyPluginButton)
-  }
-}
+    Vue.component("vue-plugin", MyPluginButton);
+  },
+};
 
+//对象类型插件
 export default MyPlugin;
+
+//函数类型插件
+// export default function (Vue) {
+//   console.log(Vue.version);
+// }
